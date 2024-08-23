c820130c22377f5bc46b972e70293eb354522017
therohk <rohk@live.com>
2024-08-23 11:45:15 +0530

    shallow api

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@ export { flattenObject, unflattenObject } from "./data-utils";
 export { deepDiffLow } from "./diff-high";
 export { deepDiffFlat } from "./diff-high";
 export { deepDiffTyped } from "./diff-high";
+
+export { UpdateCode } from "./merge-low";
+export { updateCodeInfo } from "./merge-low";
+export { MergeError } from "./merge-high";
+
+export { shallowMerge, immutableMerge } from "./merge-high";
+export { type DetailConfig, detailMerge } from "./merge-high";
+export { diffFromMerge } from "./merge-high";
+
+//defaults
+export { shallowMerge as merge } from "./merge-high";
+export { deepDiffTyped as diff } from "./diff-high";
