e82ed7bff797abb2d165be39edfac870f651dcaf
therohk <rohk@live.com>
2024-08-23 08:40:51 +0530

    strategy core

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # datum-merge
-Simplified diff and merge for deeply nested objects.
+
+**datum-merge** is a modern typescript library that simplifies merge and diff operations for deeply nested objects.
+
+**Source Code** : https://github.com/therohk/datum-merge
+
+**NPM library** : https://www.npmjs.com/package/datum-merge
+
+![](https://github.com/therohk/datum-merge/actions/workflows/build.yml/badge.svg)
+
+---
+
+## Upcoming Features
+
+1. inline entire [deep-diff](https://github.com/flitbit/diff) library which is unmaintained and contains serious bugs.
+
+2. support merging for top level arrays or primitives.
+
+3. project should compile in strict mode.
+
+4. formalize support for deeply nested objects (for v1).
+
+Code contributions are welcome via issues and pull requests.
+
+---
+
+## Examples
+
+```
+import { merge, MergeConfig, UpdateCode } from "datum-merge";
+const conf: MergeConfig = {
+    "id": UpdateCode.I,
+    "field1": UpdateCode.B,
+    "arr*": UpdateCode.XM,
+    nested: UpdatedCode.N,
+    "obj1": {
+        scalar: UpdateCode.B,
+        vector: UpdateCode.XM,
+    },
+};
+const diff = merge(target, source, conf);
+```
+
+## Merge Strategy
+
+This string code describes how modifications to an attribute for a PUT/UPDATE operation should be handled.
+It decides whether a change to the value of the field is allowed during a merge operation between two entities.
+
+### Strategy Codes
+
+The same field within a source and target object is represented by `s` and `t` respectively.
+Whether the strategy requires data to be present for the field, is shown by { 0=no, 1=yes, X=irrelavant }. 
+The value of the source field is migrated to the target field only if the predicate passes.
+See the implementation of this logic in [merge-low.ts](datum-merge/src/merge-low.ts).
+
+| Code Value | Predicate | Meaning |
+|----|----|----|
+| C | n/a | always create new instance |
+| N | `0` | reject any change, skip merge |
+| Y | `sX & tX` | accept any change ; bypass merge |
+| B | `s1 & tX` | insert or update, no delete |
+| H | `s1 & t1` | update only if exists |
+| U | `sX & t1` | update or delete only, no insert |
+| I | `sX & t0` | insert only, no update or delete |
+| D | `s0 & tX` | delete only, no update or insert |
+| XR | `sX & tX` | full vector replacement |
+| XM | `s ∪ t`   | set union, vector merge |
+| XD | `s - t`   | set difference, delete given values |
+| XI | `s ∩ t`   | set intersection, delete missing values |
+| XS | `s + t` | preserve order insert (allows dupes) |
+| XF | `t + s` | insert from start (allows dupes) |
+
+Applying the merge results in one of these transitions per primitive value in the target object.
+
+| Code Value | Meaning | Transitions |
+|----|----|----|
+| Z | noop / ignore  | `null <- null` or `non-null == non-null` |
+| N | new / insert   | `null <- non-null` |
+| E | edit / update  | `non-null <- non-null` |
+| D | unset / delete | `non-null <- null` |
+
+---
---
+++ src/merge-low.ts
@@ -0,0 +1,187 @@
+// import { get, has, set, unset } from "lodash-es";
+import { concat, differenceWith, intersectionWith, unionWith, isEqual } from "lodash-es";
+import { isArrayOfAny, isNullish } from "./type-utils";
+import { areArraysEqual, deepClone } from "./data-utils";
+
+export enum UpdateCode {
+    T = "T", //touch, blank update
+    C = "C", //create instance
+    // scalar/def codes
+    N = "N", //ignore change
+    Y = "Y", //accept any change
+    B = "B", //insert or update, no delete
+    U = "U", //update or delete only
+    H = "H", //update only if exists
+    I = "I", //insert only
+    D = "D", //delete only
+    // vector/xref codes
+    XR = "XR", //full replace
+    XM = "XM", //set union, vector merge
+    XD = "XD", //set difference, delete given values
+    XI = "XI", //set intersection, delete missing values
+    XS = "XS", //preserve order insert (allows dupes)
+    XF = "XF", //insert from start (allows dupes)
+}
+
+export function updateCodeInfo(
+    mergeCode: UpdateCode
+): {
+    insert: boolean,
+    update: boolean,
+    unset: boolean,
+    enable: boolean,
+} {
+    const allowUnset = [
+        UpdateCode.Y, UpdateCode.D, UpdateCode.U,
+        UpdateCode.XR, UpdateCode.XD, UpdateCode.XI]
+        .includes(mergeCode);
+    const allowInsert = [
+        UpdateCode.Y, UpdateCode.I, UpdateCode.B,
+        UpdateCode.XR, UpdateCode.XM, UpdateCode.XS, UpdateCode.XF]
+        .includes(mergeCode);
+    const allowUpdate = [
+        UpdateCode.Y, UpdateCode.H, UpdateCode.U, UpdateCode.B]
+        .includes(mergeCode)
+        || mergeCode.startsWith("X");
+    return {
+        insert: allowInsert,
+        update: allowUpdate,
+        unset: allowUnset,
+        enable: allowInsert || allowUpdate || allowUnset,
+    };
+}
+
+export function mergeScalarField(
+    target: any,
+    source: any,
+    label: string,
+    mergeCode: UpdateCode,
+): boolean {
+    const sourceHas = !isNullish(source[label]);
+    const targetHas = target.hasOwnProperty(label); //!isNullish(target[label]);
+    if (!targetHas && !sourceHas) {
+        return false;
+    }
+    if (targetHas && sourceHas) {
+        //lhs vs rhs type check
+        if (typeof target[label] !== typeof source[label]) {
+            throw new TypeError("field type mismatch for " + label);
+        }
+        if (target[label] === source[label]) {
+            return false;
+        }
+    }
+    let migrateVal = false;
+    switch (mergeCode) {
+        case UpdateCode.N:
+            return false;
+        case UpdateCode.Y:
+            target[label] = source[label];
+            return true;
+        case UpdateCode.B:
+            if (sourceHas) migrateVal = true;
+            break;
+        case UpdateCode.U:
+            if (targetHas) migrateVal = true;
+            break;
+        case UpdateCode.I:
+            if (!targetHas) migrateVal = true;
+            break;
+        case UpdateCode.H:
+            if (targetHas && sourceHas) migrateVal = true;
+            break;
+        case UpdateCode.D:
+            if (targetHas && !sourceHas) migrateVal = true;
+            break;
+        case UpdateCode.XR:
+        case UpdateCode.XS:
+        case UpdateCode.XF:
+        case UpdateCode.XM:
+        case UpdateCode.XD:
+        case UpdateCode.XI:
+            return mergeVectorField(target, source, label, mergeCode);
+    }
+    if (migrateVal) {
+        if (sourceHas) {
+            // isPrimitive(source[label]); //avoid cloning
+            target[label] = deepClone(source[label]);
+        } else {
+            delete target[label];
+        }
+        return true;
+    }
+    return false;
+};
+
+export function mergeVectorField(
+    target: any,
+    source: any,
+    label: string,
+    mergeCode: UpdateCode,
+): boolean {
+    let sourceVals = source[label]; //get(source, label);
+    if (isNullish(sourceVals)) {
+        return false; //sourceHas
+    }
+    let sourceVec = isArrayOfAny(sourceVals);
+    const targetHas = target.hasOwnProperty(label);
+    const targetVec = isArrayOfAny(target[label]);
+    if (targetHas && !targetVec) {
+        //should T => T[] change be allowed  
+        throw new TypeError("type change to vector for " + label);
+    }
+    //todo verify same array types
+    if (!sourceVec) {
+        sourceVals = [sourceVals] as unknown[];
+        sourceVec = true;
+    }
+
+    let targetVals: unknown[];
+    switch (mergeCode) {
+        case UpdateCode.N:
+            return false;
+        case UpdateCode.Y:
+        case UpdateCode.XR:
+            target[label] = deepClone(sourceVals);
+            return true;
+        case UpdateCode.XS:
+            if (!targetHas)
+                target[label] = [];
+            target[label].push(...sourceVals);
+            return true;
+        // targetVals = concat(target[label] ?? [], sourceVals);
+        // break;
+        case UpdateCode.XF:
+            //entire array appears in diff
+            if (!targetHas)
+                target[label] = [];
+            target[label].unshift(...sourceVals);
+            return true;
+        // targetVals = concat(sourceVals, target[label] ?? []);
+        // break;
+        case UpdateCode.B:
+        case UpdateCode.XM:
+            targetVals = unionWith(target[label], sourceVals, isEqual);
+            break;
+        // case UpdateCode.D:
+        case UpdateCode.XD:
+            targetVals = differenceWith(target[label], sourceVals, isEqual);
+            break;
+        case UpdateCode.XI:
+            targetVals = intersectionWith(target[label], sourceVals, isEqual);
+            break;
+        default:
+            return false;
+    }
+    if (!targetVals?.length) {
+        delete target[label];
+        return true;
+    }
+
+    const changed = !areArraysEqual(targetVals, target[label]);
+    // && !deepEquals(targetVals, target[label]);
+    //always clone source objects?
+    target[label] = targetVals; //array replaced
+    // set(target, label, targetVals);
+    return changed; //was always true
+}
