e66c653fbd6a4d81868a63de068164acd66919af
therohk <rohk@live.com>
2024-08-25 07:15:05 +0530

    datum checks

--- src/datum-utils.ts
+++ src/datum-utils.ts
@@ -11,9 +11,8 @@ export function getObjectKeys(
     }
     let sourceKeys = Object.keys(obj);
     if (includeKeys && !!includeKeys.length) {
-        //dont add duplicates
         includeKeys.filter((k) => !sourceKeys.includes(k))
-            .forEach((k) => sourceKeys.push(k));
+            .forEach((k) => sourceKeys.push(k)); //no dupes
     }
     if (excludeKeys && !!excludeKeys.length) {
         sourceKeys = sourceKeys.filter((k) => !excludeKeys.includes(k));
@@ -48,35 +47,6 @@ export function toUniqueArray<T>(arr: T[]): T[] {
     return [...new Set<T>(arr)];
 }
 
-export function containsAny<T>(
-    needles: T[],
-    haystack: T[],
-): boolean {
-    if (!needles?.length || !haystack?.length) {
-        return false;
-    }
-    return needles.some((i) => haystack.includes(i));
-}
-
-export function containsAll<T>(
-    needles: T[],
-    haystack: T[],
-): boolean {
-    if (!needles?.length || !haystack?.length) {
-        return false;
-    }
-    return needles.every((i) => haystack.includes(i));
-}
-
-function arrayStartsWith<T>(arr: T[], base: T[]): boolean {
-    for (let i = 0; i < base.length; i++) {
-        if (arr[i] !== base[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 export function areArraysEqual<T>(
     arr1: T[] | undefined,
     arr2: T[] | undefined,
@@ -95,45 +65,12 @@ export function areArraysEqual<T>(
     return true;
 }
 
-function deleteEmptyKeys(obj: any): void {
-    //modifies passed obj in place
-    //removes empty properties and empty arrays
-    return Object.keys(obj).forEach((k) => {
-        if (!obj[k] || obj[k] === undefined
-            || (Array.isArray(obj[k]) && obj[k].length === 0)) {
-            delete obj[k];
-        }
-    });
-}
-
 //-----------------------------------------------------------------------------
 
-function selectKeys(
-    obj: any,
-    excludeKeys: string[]
-): any {
-    //creates new object
-    return Object.fromEntries(
-        Object.entries(obj)
-            .filter(([k, _]) => !excludeKeys.includes(k))
-    );
-}
-
-// function clonePartial<T>(obj: T, fields?: string[]): Partial<T> {
-//     if (!obj) {
-//         return obj;
-//     }
-//     fields = fields || Object.keys(obj);
-//     return fields.reduce((clone, field) => {
-//         clone[field] = deepClone(obj[field]);
-//         return clone;
-//     }, {} as Partial<T>);
-// }
-
 export function flattenObject(
-    obj: { [key:string]: any }
-): { [key:string]: any } {
-    const flatten: { [key:string]: any } = {};
+    obj: { [key: string]: any }
+): { [key: string]: any } {
+    const flatObj: { [key: string]: any } = {};
     const path: any[] = [];
     const isObject = (value: any) => Object(value) === value;
     function dig(obj: any) {
@@ -142,22 +79,22 @@ export function flattenObject(
             if (isObject(value)) {
                 dig(value);
             } else {
-                flatten[path.join('.')] = value;
+                flatObj[path.join('.')] = value;
             }
             path.pop();
         }
     }
     dig(obj);
-    return flatten;
+    return flatObj;
 }
 
 export function unflattenObject(
-    flatObj: { [key:string]: any }
-): { [key:string]: any } {
-    const unFlatten: { [key:string]: any } = {};
+    flatObj: { [key: string]: any }
+): { [key: string]: any } {
+    const unflatObj: { [key: string]: any } = {};
     for (const [path, value] of Object.entries(flatObj)) {
         const chain = path.split('.');
-        let object = unFlatten;
+        let object = unflatObj;
         for (const [i, key] of chain.slice(0, -1).entries()) {
             if (!object[key]) {
                 const needArray = Number.isInteger(Number(chain[+i + 1]));
@@ -168,5 +105,5 @@ export function unflattenObject(
         const lastkey = chain.pop();
         object[lastkey!] = value;
     }
-    return unFlatten;
+    return unflatObj;
 }
