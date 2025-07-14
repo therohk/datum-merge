bef0b083534ae701698c8f169b0035a957418c99
therohk <rohk@live.com>
2025-07-14 19:27:59 +0530

    support browser

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,16 +2,17 @@ import esbuild from "esbuild";
 import process from "process";
 
 esbuild.build({
-	entryPoints: ['deep-diff.ts'],
-	tsconfig: 'tsconfig.lib.json',
+	entryPoints: ['src/index.ts'],
+	tsconfig: 'tsconfig.json',
 	bundle: true,
 	platform: 'browser',
-	format: 'cjs',
+	format: 'iife',
 	target: 'es6',
 	external: [],
 	logLevel: 'info',
 	sourcemap: 'external',
 	minify: true,
 	treeShaking: false,
-	outfile: 'dist-diff/umd/deep-diff.min.js',
+	legalComments: 'none',
+	outfile: 'dist/umd/datum-merge.min.js',
 }).catch(() => process.exit(1));
